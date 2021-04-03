a63cc48593483129325d586f8b87072c735e3460
occiandiaali <ocean.diaali@gmail.com>
2021-04-03 16:43:10 +0100

    initial commit

---
+++ lib/store/cart.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mirus_global/config/config.dart';
+//import 'package:mirus_global/address/address.dart';
+import 'package:mirus_global/widgets/loadingWidget.dart';
+import 'package:mirus_global/models/item.dart';
+import 'package:mirus_global/counters/cartitemcounter.dart';
+//import 'package:mirus_global/counters/totalMoney.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:mirus_global/store/storehome.dart';
+import 'package:provider/provider.dart';
+import '../main.dart';
+
+class CartPage extends StatefulWidget {
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
