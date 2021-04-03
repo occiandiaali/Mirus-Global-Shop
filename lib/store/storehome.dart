042ca0c00ea9b2fe014ae2cea8b9c0fc9bd64597
occiandiaali <ocean.diaali@gmail.com>
2021-04-03 16:44:25 +0100

    add appbar and drawer

--- lib/store/storehome.dart
+++ lib/store/storehome.dart
@@ -25,8 +25,76 @@ class _StoreHomeState extends State<StoreHome> {
     width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.black12, Colors.blueGrey],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp,
+              ),
+            ),
+          ),
+          title: Text(
+            'MG Shop',
+            style: TextStyle(
+              fontSize: 35.0,
+              color: Colors.white,
+              fontFamily: 'Dancing Script',
+            ),
+          ),
+          centerTitle: true,
+          actions: [
+            Stack(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.shopping_cart_outlined, color: Colors.white,),
+                  onPressed: () {
+                    Route route = MaterialPageRoute(builder: (c) => CartPage());
+                    Navigator.pushReplacement(context, route);
+                  },
+                ),
+                Positioned(
+                  child: Stack(
+                    children: [
+                      Icon(
+                        Icons.brightness_1,
+                        size: 20.0,
+                        color: Colors.blue,
+                      ),
+                      Positioned(
+                        top: 3.0,
+                        bottom: 4.0,
+                        child: Consumer<CartItemCounter>(
+                          builder: (context, counter, _) {
+                            return Text(
+                              counter.count.toString(),
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12.0,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        drawer: MyDrawer(),
         body: Center(
-          child: Text('Welcome to your Home Screen'),
+          child: Text(
+            'Welcome to your Home Screen',
+            style: TextStyle(
+              fontSize: 25.0
+            ),
+          ),
         ),
       ),
     );
