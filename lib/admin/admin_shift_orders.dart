96b28fe95edb6219b52a57b38ebb6b60c0aafdca
occiandiaali <ocean.diaali@gmail.com>
2021-04-05 13:05:04 +0100

    initial commit

---
+++ lib/admin/admin_shift_orders.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:mirus_global/admin/adminOrderCard.dart';
+import 'package:mirus_global/config/config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../widgets/loadingWidget.dart';
+//import '../widgets/orderCard.dart';
+
+class AdminShiftOrders extends StatefulWidget {
+  @override
+  _MyOrdersState createState() => _MyOrdersState();
+}
+
+
+class _MyOrdersState extends State<AdminShiftOrders> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+    );
+  }
+}
