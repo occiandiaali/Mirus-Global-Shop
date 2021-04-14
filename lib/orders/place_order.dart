42427103a6c324d4b7e264af61daae1e028598aa
occiandiaali <ocean.diaali@gmail.com>
2021-04-14 20:01:09 +0100

    first commit

---
+++ lib/orders/place_order.dart
@@ -0,0 +1,21 @@
+import 'package:mirus_global/config/config.dart';
+import 'package:mirus_global/store/storehome.dart';
+import 'package:mirus_global/counters/cartitemcounter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PaymentPage extends StatefulWidget {
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+
+
+
+class _PaymentPageState extends State<PaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material();
+  }
+}
