4aefe9781bcf86dacd81aff55405a180225f2d88
occiandiaali <ocean.diaali@gmail.com>
2021-04-15 18:53:32 +0100

    first commit

--- lib/address/add_address.dart
+++ lib/address/add_address.dart
@@ -1,24 +1,133 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:mirus_global/Widgets/customAppBar.dart';
 import 'package:mirus_global/config/config.dart';
 //import 'package:mirus_global/widgets/customAppBar.dart';
 //import 'package:mirus_global/models/address.dart';
 import 'package:flutter/material.dart';
+import 'package:mirus_global/models/address.dart';
 
 class AddAddress extends StatelessWidget {
+
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final streetAddressController = TextEditingController();
+  final cityController = TextEditingController();
+  final stateController = TextEditingController();
+  final countryController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        key: scaffoldKey,
+        appBar: MyAppBar(),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {
+              if(formKey.currentState.validate()) {
+                final model = AddressModel(
+                  name: nameController.text.trim(),
+                  phoneNumber: phoneController.text.trim(),
+                  street: streetAddressController.text.trim(),
+                  city: cityController.text.trim(),
+                  state: stateController.text.trim(),
+                  country: countryController.text.trim(),
+                ).toJson();
+                // add to fire store
+                EshopApp.firestore.collection(EshopApp.collectionUser)
+                    .doc(EshopApp.sharedPreferences.getString(EshopApp.userUID))
+                    .collection(EshopApp.subCollectionAddress)
+                    .doc(DateTime.now().millisecondsSinceEpoch.toString())
+                    .set(model)
+                    .then((v) {
+                  Fluttertoast.showToast(msg: 'Address added.');
+                      FocusScope.of(context).requestFocus(FocusNode());
+                      formKey.currentState.reset();
+                });
+              }
+            },
+            label: Text('Done'),
+        backgroundColor: Colors.deepPurpleAccent,
+        icon: Icon(Icons.check_outlined),),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                      'New Address',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),),
+                ),
+              ),
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    MyTextField(
+                      hint: "Name e.g. Amaka Musa Segun",
+                      controller: nameController,
+                    ),
+                    MyTextField(
+                      hint: "Phone e.g. +234-8034096604",
+                      controller: phoneController,
+                    ),
+                    MyTextField(
+                      hint: "Street e.g. 10, Good street",
+                      controller: streetAddressController,
+                    ),
+                    MyTextField(
+                      hint: "City (or Local government area name)",
+                      controller: cityController,
+                    ),
+                    MyTextField(
+                      hint: "State",
+                      controller: stateController,
+                    ),
+                    MyTextField(
+                      hint: "Country",
+                      controller: countryController,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
-}
+} // address class
+
 
 class MyTextField extends StatelessWidget {
+
+  final String hint;
+  final TextEditingController controller;
+
+  MyTextField({
+    Key key,
+    this.hint,
+    this.controller}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-
+      padding: EdgeInsets.all(8.0),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration.collapsed(
+            hintText: hint),
+        validator: (value) => value.isEmpty ?
+        "Field is required" : null,
+      ),
     );
   }
 }
--- lib/address/address.dart
+++ lib/address/address.dart
@@ -1,19 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mirus_global/counters/address_changer.dart';
+import 'package:mirus_global/orders/order_payment.dart';
+import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mirus_global/address/add_address.dart';
 import 'package:mirus_global/config/config.dart';
+import 'package:mirus_global/models/address.dart';
 import 'package:mirus_global/orders/place_order.dart';
 import 'package:mirus_global/Widgets/customAppBar.dart';
 import 'package:mirus_global/Widgets/loadingWidget.dart';
-//import 'package:mirus_global/Widgets/wideButton.dart';
-//import 'package:mirus_global/models/address.dart';
-//import 'package:mirus_global/counters/changeAddresss.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:mirus_global/Widgets/wideButton.dart';
 
-//import 'addAddress.dart';
+
+
 
 class Address extends StatefulWidget {
+
   final double totalAmount;
+
   const Address({Key key, this.totalAmount}) : super(key: key);
+
   @override
   _AddressState createState() => _AddressState();
 }
@@ -23,29 +29,204 @@ class _AddressState extends State<Address> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      child: Scaffold(
+        appBar: MyAppBar(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Select Address',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Consumer<AddressChanger>(
+              builder: (context, address, c) {
+                return Flexible(
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: EshopApp.firestore
+                        .collection(EshopApp.collectionUser)
+                        .doc(EshopApp.sharedPreferences.getString(EshopApp.userUID))
+                        .collection(EshopApp.subCollectionAddress).snapshots(),
 
+                    builder: (context, snapshot) {
+                      return !snapshot.hasData ?
+                          Center(child: circularProgress(),)
+                          : snapshot.data.docs.length == 0 ?
+                          noAddressCard() : ListView.builder(
+                        itemCount: snapshot.data.docs.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return AddressCard(
+                            currentIndex: address.count,
+                            value: index,
+                            addressId: snapshot.data.docs[index].id,
+                            totalAmount: widget.totalAmount,
+                            model: AddressModel.fromJson(snapshot.data.docs[index].data()),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          elevation: 10.0,
+          label: Text('Add address'),
+          backgroundColor: Colors.deepPurpleAccent,
+          icon: Icon(Icons.add_location_alt_outlined),
+          onPressed: () {
+            Route route = MaterialPageRoute(builder: (c) => AddAddress());
+            Navigator.push(context, route);
+          },
+            ),
+      ),
     );
   }
 
   noAddressCard() {
     return Card(
-
+      color: Colors.purple.withOpacity(0.5),
+      child: Container(
+        height: 100.0,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+                Icons.add_location_alt_outlined,
+            color: Colors.white,),
+            Text('Please add your address for delivery'),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class AddressCard extends StatefulWidget {
+  final AddressModel model;
+  final String addressId;
+  final double totalAmount;
+  final int currentIndex;
+  final int value;
+
+  AddressCard({
+    Key key,
+  this.model,
+  this.addressId,
+  this.totalAmount,
+  this.currentIndex,
+  this.value}) : super(key: key);
 
   @override
   _AddressCardState createState() => _AddressCardState();
 }
 
 class _AddressCardState extends State<AddressCard> {
+
   @override
   Widget build(BuildContext context) {
-
+    double screenWidth = MediaQuery.of(context).size.width;
     return InkWell(
-
+      onTap: () => Provider.of<AddressChanger>(context, listen: false)
+            .displayResult(widget.value),
+      child: Card(
+        color: Colors.purpleAccent.withOpacity(0.4),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Radio(
+                  groupValue: widget.currentIndex,
+                  value: widget.value,
+                  activeColor: Colors.purple,
+                  onChanged: (value) {
+                    Provider.of<AddressChanger>(
+                        context,
+                        listen: false)
+                        .displayResult(value);
+                  },
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10.0),
+                      width: screenWidth * 0.8,
+                      child: Table(
+                        children: [
+                          TableRow(
+                            children: [
+                              KeyText(msg: 'Name',),
+                              Text(widget.model.name),
+                            ],
+                          ),
+                          TableRow(
+                            children: [
+                              KeyText(msg: 'Phone',),
+                              Text(widget.model.phoneNumber),
+                            ],
+                          ),
+                          TableRow(
+                            children: [
+                              KeyText(msg: 'Street',),
+                              Text(widget.model.street),
+                            ],
+                          ),
+                          TableRow(
+                            children: [
+                              KeyText(msg: 'City',),
+                              Text(widget.model.city),
+                            ],
+                          ),
+                          TableRow(
+                            children: [
+                              KeyText(msg: 'State',),
+                              Text(widget.model.state),
+                            ],
+                          ),
+                          TableRow(
+                            children: [
+                              KeyText(msg: 'Country',),
+                              Text(widget.model.country),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            widget.value == Provider.of<AddressChanger>(context).count ?
+                WideButton(
+                  message: 'Proceed',
+                  onPressed: () {
+                    Route route = MaterialPageRoute(
+                      builder: (c) => OrderPayment(
+                        addressId: widget.addressId,
+                        totalAmount: widget.totalAmount,
+                      ),
+                    );
+                    Navigator.push(context, route);
+                  },
+                ) : Container(),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -55,9 +236,18 @@ class _AddressCardState extends State<AddressCard> {
 
 
 class KeyText extends StatelessWidget {
+  final String msg;
+
+  KeyText({Key key, this.msg}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text("");
+    return Text(
+      msg,
+      style: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      ),
+    );
   }
 }
